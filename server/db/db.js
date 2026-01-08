aa858ff5d2a0cbc5cf973f361647c5ec08294662
TanmayT134 <tawade.tanmay134@gmail.com>
2026-01-08 16:44:07 +0530

    Update API URL for production

--- server/db/db.js
+++ server/db/db.js
@@ -9,11 +9,12 @@ console.log("DB_NAME =", process.env.DB_NAME);
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
 
 
 db.connect((err) => {
