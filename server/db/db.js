a26a0a3cc4a42e94b0da3895dec22db361e8a149
TanmayT134 <tawade.tanmay134@gmail.com>
2026-01-08 16:12:29 +0530

    Fix Railway DB connection for Render using public proxy

--- server/db/db.js
+++ server/db/db.js
@@ -9,10 +9,12 @@ console.log("DB_NAME =", process.env.DB_NAME);
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
     if (err) {
